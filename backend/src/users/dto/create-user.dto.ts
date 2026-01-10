58146526fbb133fb0f903707baa66677886bc0ea
dNeRat <d.oklad03@gmail.com>
2026-01-10 16:31:56 +0700

    add dto validation

--- backend/src/users/dto/create-user.dto.ts
+++ backend/src/users/dto/create-user.dto.ts
@@ -1,4 +1,28 @@
+import { IsEmail, IsOptional, IsString, MinLength } from 'class-validator';
+
 export class CreateUserDto {
-    id: number;
-    name: string;
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(6)
+  password: string;
+
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsOptional()
+  @IsString()
+  avatarUrl?: string;
+
+  @IsOptional()
+  @IsString()
+  position?: string;
+
+  @IsOptional()
+  @IsString()
+  department?: string;
 }
--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,5 +1,6 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Controller('users')
 export class UsersController {
@@ -11,18 +12,7 @@ export class UsersController {
   }
 
   @Post()
-  async createUser(
-    @Body()
-    body: {
-      email: string;
-      password: string;
-      firstName: string;
-      lastName: string;
-      avatarUrl?: string;
-      position?: string;
-      department?: string;
-    },
-  ) {
+  async createUser(@Body() body: CreateUserDto) {
     return this.usersService.create(body);
   }
 }
